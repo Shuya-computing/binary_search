2c67cf23b5afd105180040e9456f3589b0a654f2
Shuya Hanai <hanatama2gou@gmail.com>
2020-06-30 01:54:31 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -3,7 +3,14 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x){
+  int sum = 0;
+  int i = 0;
+  for (i = 0; i < n; i++){
+    sum = sum + ( A[i] / x );
+  }
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +18,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000001;
+  while (ub - lb >1){
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      lb = mid;
+    }
+    else {
+      ub = mid;
+    }
+  }
+  printf("%d\n", lb);
   return 0;
 }
--- src/works.c
+++ src/works.c
@@ -3,7 +3,23 @@
 int n;
 int k;
 int A[100000];
-
+int p(int x){
+  int j = 0;
+  //最終日の仕事をした人の人数を考えるために、最初から人数を1にしておく。
+  int human = 1;
+  int worklog = 0;
+  for(j = 0; j < n; j++){
+    worklog = worklog + A[j];
+    if (worklog > x){
+      return 0;
+    }
+    if ((worklog+A[j+1]) > x){
+      human = human + 1;
+      worklog = 0;
+    }
+  }
+  return human <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +27,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while (ub - lb > 1 ){
+    int mid = (lb + ub) / 2;
+    if (p(mid)){
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n" , ub);
   return 0;
 }
